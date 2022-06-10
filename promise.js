9f0c1df16d2b470c25b402dd274c352655662739
Hanif <hanif.hakim@spesolution.com>
2022-06-10 16:22:12 +0700

    Finisihed Challenge with both happy and sad

--- promise.js
+++ promise.js
@@ -1,7 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (emotion) => {
+  try {
+    // Call Promise
+    const IXX = await promiseTheaterIXX();
+    const VGC = await promiseTheaterVGC();
+
+    // Mix Result Promise
+    const arrMix = [...IXX, ...VGC];
+
+    // Filtered Marah dan Tidak Marah
+    let marah = arrMix.filter((o) => o.hasil === "marah");
+    let tidakMarah = arrMix.filter((o) => o.hasil === "tidak marah");
+
+    // Membuat Promise, LETS GO!!!
+    return new Promise((resolve, project) => {
+      if (emotion === "marah") {
+        resolve(marah.length);
+      }
+      if (emotion === "tidak marah") {
+        resolve(tidakMarah.length);
+      }
+    });
+  } catch (rejectMessage) {
+    console.log(rejectMessage);
+  }
+};
 
 module.exports = {
   promiseOutput,
